4ce2c308a436987dd264ab3f2a3e129344ae5f5b
akashsarkar99999 <dfakash99999@gmail.com>
2024-09-03 14:21:41 +0600

    third commit

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+document.getElementById('buy-tickets-btn').addEventListener('click', function(){
+   const mainSection = document.getElementById('main-section')
+   window.scrollBy(0, 1600)
+    
+})
+
+function clicked(buttonId){
+    const perticulerButton = document.getElementById(buttonId)
+    perticulerButton.style.backgroundColor = '#1DD100'
+
+    
+
+    }
+
+   
+
+
